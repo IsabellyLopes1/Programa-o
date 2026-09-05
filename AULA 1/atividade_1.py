7cbd8007b7c060aacaf911866b70d406659ce6c4
IsabellyLopes1 <bellysantoslopes@gmail.com>
2026-09-05 17:04:05 -0300

    Add files via upload

---
+++ AULA 1/atividade_1.py
@@ -0,0 +1,77 @@
+import streamlit as st
+
+st.title("Meu Cartão de Visitas")
+
+st.header("Sobre Mim")
+
+st.markdown("Eu me chamo Isabelly e estou aprendendo a criar aplicações com Python e Streamlit.")
+
+st.header("Minhas Habilidades")
+
+st.text("Banco de Dados para Data Sciece")
+
+st.header("Contato")
+
+st.markdown(" Email: **bellysantoslopes@gmail.com**")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
